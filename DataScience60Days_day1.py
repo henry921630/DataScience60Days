b18d60c959565e95e56c5f0f49d2ee2a3f9078a3
henry921630 <henry921630@gmail.com>
2021-01-18 12:44:36 +0800

    Add files via upload

---
+++ DataScience60Days_day1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[55]:
+
+
+import numpy as np
+
+
+# In[56]:
+
+
+#1.生成一個等差數列，首數為0，尾數為20，公差為1的數列。
+a = np.arange(21)
+a
+
+
+# In[57]:
+
+
+#2.呈上題，將以上數列取出偶數。
+b = np.arange(2,21,2)
+b
+
+
+# In[58]:
+
+
+#3.呈1題，將數列取出3的倍數。
+c = np.arange(3,21,3)
+c
+
